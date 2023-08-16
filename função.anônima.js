08cecf6b24a8f2feb38faf777847f6bad79ed6c7
JanineCruz921 <cruzjanine921@gmail.com>
2023-08-16 18:29:56 -0300

    Atividades da Aula - Funções anônimas

---
+++ função.anônima.js
@@ -0,0 +1,14 @@
+var valor1 = 5
+var valor2 = 3
+
+function funcaoSimplesSubtracao(a, b) {
+    return a - b
+}
+console.log(funcaoSimplesSubtracao(valor1,valor2))
+
+const funcaoAnonimaSoma = function (a, b) {
+    return a + b
+}
+console.log(funcaoAnonimaSoma(valor1, valor2))
+
+
